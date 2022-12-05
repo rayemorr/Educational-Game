3351f170fa5608f5d2db6da45b564ca8be6e1160
Rachael <rayemorr@gmail.com>
2022-12-05 14:40:49 -0600

    Added walking animation; updating documentation

--- code/player.py
+++ code/player.py
@@ -12,8 +12,7 @@ class Player(pygame.sprite.Sprite):
         self.frame_index = 0
 
         # general setup
-        self.image = pygame.Surface((32, 64))
-        self.image.fill('green')
+        self.image = self.animations[self.status][self.frame_index]
         self.rect = self.image.get_rect(center=pos)
 
         # movement attributes
@@ -23,29 +22,45 @@ class Player(pygame.sprite.Sprite):
 
     def import_assets(self):
         self.animations = {'up': [], 'down': [], 'left': [], 'right': [],
-                           'up_idle': [], 'down_idle:': [], 'left_idle': [], 'right_idle': []}
+                           'up_idle': [], 'down_idle': [], 'left_idle': [], 'right_idle': []}
 
         for animation in self.animations.keys():
             full_path = '..\\CPSC60500-Project\\graphics\\player1\\' + animation
             self.animations[animation] = import_folder(full_path)
         print(self.animations)
 
+    def animate(self, dt):
+        self.frame_index += 4 * dt
+        if self.frame_index >= len(self.animations[self.status]):
+            self.frame_index = 0
+
+        self.image = self.animations[self.status][int(self.frame_index)]
+
     def input(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_UP]:
             self.direction.y = -1
+            self.status = 'up'
         elif keys[pygame.K_DOWN]:
             self.direction.y = 1
+            self.status = 'down'
         else:
             self.direction.y = 0
 
         if keys[pygame.K_RIGHT]:
             self.direction.x = 1
+            self.status = 'right'
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.status = 'left'
         else:
             self.direction.x = 0
+
+    def get_status(self):
+        # if player is not moving, add _idle to status
+        if self.direction.magnitude() == 0:
+            self.status = self.status.split('_')[0] + '_idle'
 
     def move(self, dt):
         # normalizing a vector
@@ -61,4 +76,7 @@ class Player(pygame.sprite.Sprite):
 
     def update(self, dt):
         self.input()
+        self.get_status()
+
         self.move(dt)
+        self.animate(dt)
