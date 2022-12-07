ccbcf905f69503de67c5872a9838a612ac7f0a9c
Rachael <rayemorr@gmail.com>
2022-12-07 17:27:32 -0600

    randomized player movement; print out the time and amount of moves the player has made

--- code/player.py
+++ code/player.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 from settings import *
 from support import *
@@ -20,6 +22,13 @@ class Player(pygame.sprite.Sprite):
         self.pos = pygame.math.Vector2(self.rect.center)
         self.speed = 200
 
+        self.time_elapsed = 0
+        self.game_time = 0
+
+        self.curr_x = self.pos.x
+        self.curr_y = self.pos.y
+        self.moves = 0
+
     def import_assets(self):
         self.animations = {'up': [], 'down': [], 'left': [], 'right': [],
                            'up_idle': [], 'down_idle': [], 'left_idle': [], 'right_idle': []}
@@ -36,31 +45,68 @@ class Player(pygame.sprite.Sprite):
 
         self.image = self.animations[self.status][int(self.frame_index)]
 
-    def input(self):
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_UP]:
-            self.direction.y = -1
-            self.status = 'up'
-        elif keys[pygame.K_DOWN]:
-            self.direction.y = 1
-            self.status = 'down'
-        else:
-            self.direction.y = 0
-
-        if keys[pygame.K_RIGHT]:
-            self.direction.x = 1
-            self.status = 'right'
-        elif keys[pygame.K_LEFT]:
-            self.direction.x = -1
-            self.status = 'left'
-        else:
-            self.direction.x = 0
+    # def input(self):
+    #     keys = pygame.key.get_pressed()
+    #
+    #     if keys[pygame.K_UP]:
+    #         self.direction.y = -1
+    #         self.status = 'up'
+    #     elif keys[pygame.K_DOWN]:
+    #         self.direction.y = 1
+    #         self.status = 'down'
+    #     else:
+    #         self.direction.y = 0
+    #
+    #     if keys[pygame.K_RIGHT]:
+    #         self.direction.x = 1
+    #         self.status = 'right'
+    #     elif keys[pygame.K_LEFT]:
+    #         self.direction.x = -1
+    #         self.status = 'left'
+    #     else:
+    #         self.direction.x = 0
 
     def get_status(self):
-        # if player is not moving, add _idle to status
+        # idle
         if self.direction.magnitude() == 0:
             self.status = self.status.split('_')[0] + '_idle'
+
+    def move_randomly(self, dt):
+        self.time_elapsed += dt
+
+        if self.time_elapsed > 2:
+            rand = random.randint(0, 3)
+            # print("rand: " + str(rand))
+
+            if rand == 0:
+                self.direction.y = -1
+                self.status = 'up'
+            elif rand == 1:
+                self.direction.y = 1
+                self.status = 'down'
+
+            if rand == 2:
+                self.direction.x = 1
+                self.status = 'right'
+            elif rand == 3:
+                self.direction.x = -1
+                self.status = 'left'
+
+            self.time_elapsed = 0
+
+        if self.curr_x - self.pos.x >= 32 or self.curr_x - self.pos.x <= -32:
+            self.direction.x = 0
+            self.curr_x = self.pos.x
+            self.moves += 1
+            print("moves: " + str(self.moves))
+            print("time: " + str(self.game_time))
+
+        if self.curr_y - self.pos.y >= 32 or self.curr_y - self.pos.y <= -32:
+            self.direction.y = 0
+            self.curr_y = self.pos.y
+            self.moves += 1
+            print("moves: " + str(self.moves))
+            print("time: " + str(self.game_time))
 
     def move(self, dt):
         # normalizing a vector
@@ -75,8 +121,10 @@ class Player(pygame.sprite.Sprite):
         self.rect.centery = self.pos.y
 
     def update(self, dt):
-        self.input()
+        #self.input()
         self.get_status()
+        self.game_time += dt
 
+        self.move_randomly(dt)
         self.move(dt)
         self.animate(dt)
